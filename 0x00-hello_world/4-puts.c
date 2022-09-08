e7b29f434334b0314c410002f52dadaec6b0880c
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-08 15:46:28 -0700

    5-prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,16 +2,11 @@
 /**
  * main - Entry point
  *
- * Return : Always 0 - (Success)
+ * Return : Always 0 (Success)
  */
 int main(void)
-/**
- * return : value of main 0
- */
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	/**
-	 * return: value for main() shows how the program exited
-	 */
+
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * return : always 0 - (success)
+ * return : Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n,");
+	printf("with proper grammar, but the outcome is a piece of art\,");
 
 	return (0);
 }
