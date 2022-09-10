9d1b0e3e9e8d1654e023bc934c4e4d1a49028178
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-09 18:01:21 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,7 +7,20 @@
  */
 int main(void)
 {
+	int n;
 	char c;
 
-	for (
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
