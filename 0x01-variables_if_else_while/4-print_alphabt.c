ae3aa857775eecbb3eda577e00ca94d5db8987e0
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-12 05:27:35 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Prints some letters of the alphabet
+ * main - This is the entry point of the code
  *
- * Return : Always 0 (Success)
+ * Description: This program prints alphabets in lowercase
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
