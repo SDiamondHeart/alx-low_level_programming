e6e5840b00335c27fd05fa18516896c17cd6d7f7
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-13 16:13:00 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _is lower - This is the user defined function for the code
+ * _islower - This is the user defined function for the code
  * @c: This is the character to print
  *
  * Description: This code writes a function to check for lower case characters
