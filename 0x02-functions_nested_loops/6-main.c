45bfa6bd514946ac9c7103dff657eabc058c116e
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-13 17:15:05 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the code
+ *
+ * Description: This program computes the absolute value of an integer
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	_abs(0);
+
+	return (0);
+}
