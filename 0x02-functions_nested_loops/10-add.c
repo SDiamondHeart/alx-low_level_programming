5199488ff75dfdc77cad2e22c8aa5c0c7f3a704f
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-13 18:59:52 -0700

    10 Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - This is the user defined function for the code
+ * @i: This is the first integer to add
+ * @j: This is the second integer to add
+ *
+ * Description: This codes adds two intergers and returns the result
+ *
+ * Return: The sum of the integers
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - This is the main entry point of the code
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	add(8, 7);
+
+	return (15);
+}
