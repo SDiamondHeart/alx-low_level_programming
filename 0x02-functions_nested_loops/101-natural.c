d8dc71e44b3996c4165e524c62562a26681051f6
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-13 20:13:25 -0700

    101 N

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the code
+ *
+ * Description: This code prints the multiples of 3 0r 5 below 1024
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	sum = 0;
+
+	for (i = 0; i < 1024; 1++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
