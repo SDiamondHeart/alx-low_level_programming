668fb02846caf49a30601fa01277fea3528c3ef9
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-13 21:34:16 -0700

    100 PTT

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,15 +11,13 @@
 
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int res;
+	int i, j, res;
 
 	if (!(n > 15 || n < 0))
 	{
-		for (i = 0; i <= 0; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= 0; j++)
+			for (j = 0; j <= n; j++)
 			{
 				res = (i * j);
 
@@ -47,9 +45,7 @@ void print_times_table(int n)
 					_putchar((res % 10) + '0');
 				}
 				else
-				{
 					_putchar((res % 10) + '0');
-				}
 			}
 			_putchar('\n');
 		}
