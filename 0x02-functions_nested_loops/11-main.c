4b3afc18b2e6446d5e8911114eab6d97e4993175
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-13 19:25:06 -0700

    11 PT 98

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - This is the entry point of the code
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	print_to_98(10);
+
+	return (0);
+}
