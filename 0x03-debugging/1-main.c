b76829c1346a81c228dc1273e986157f0e33b8d7
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-14 09:56:31 -0700

    main 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,7 +13,7 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10) /* This statement is always true, infinite loop */
+	while (i < 0) /* This statement is always true, infinite loop */
 	{
 		putchar(i);
 	}
