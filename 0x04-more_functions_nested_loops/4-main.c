e5d64415ea534567d74628f5981061a577fe609f
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-15 16:43:16 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - this is entry point of the code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_most_numbers();
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - this is a user defined function
+ *
+ * Description: this code prints most of the numbers from 0 to 9
+ *
+ * Return: 0 (success)
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+
+	_putchar('\n');
+}
