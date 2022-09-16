3c7d984d556d29f1d9d86de30c77f890d277d2fa
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-15 17:34:47 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - entry point of the code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	more_numbers();
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - this is a user defined function
+ *
+ * Description: this code prints 10 times the numbers from 0 to 14
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar('1');
+				_putchar(j % 10 + '0');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
