23777ba0ff7de9b48b489b75d823360fcfc4db54
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-15 17:03:31 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - this is the entry point of the code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096));
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - this is a user defined function
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Description: this code multiplies two integers
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
