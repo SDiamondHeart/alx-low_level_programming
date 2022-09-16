ef46ebbc7f5d244805903db1ce10e32b51e7a879
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-16 01:07:46 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry point of the code
+ *
+ * Description: calling the user defined function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_triangle(2);
+	print_triangle(10);
+	print_triangle(1);
+	print_triangle(0);
+	print_triangle(-4);
+
+	return (0);
+}
