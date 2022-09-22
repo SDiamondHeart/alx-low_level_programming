24cfd326b6c92118c9c6334b839b413830dbc836
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-22 00:24:46 -0700

    done with mandatory tasks

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strncat - function that concatenates two strings
+ * @dest: string to append src
+ * @src: string to append dest
+ * @n: largest number of bytes to append
+ *
+ * Return: address to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = j = 0;
+	while (*(dest + i))
+		i++;
+	while (j < n && *(src + j))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	if (j < n)
+		*(dest + i) = *(src + j);
+
+	return (dest);
+}
