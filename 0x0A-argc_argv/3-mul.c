0b59ec6c21f1503449eb4dc931ea79f1d6efd88a
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-09-30 13:48:28 -0700

    tasks

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints a program that multiplies two numbers
+ * @argc: int
+ * @argv: lists
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int mult;
+
+	if (argc == 3)
+	{
+		mult = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mult);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
