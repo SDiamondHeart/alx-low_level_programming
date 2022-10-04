f266862ef57f3617b1f3de8a7322cfb74ca492dd
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-10-04 04:44:57 -0700

    tasks

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -12,17 +12,9 @@
 
 void free_grid(int **grid, int height)
 {
-	int y;
+	int i = 0;
 
-	if (grid == NULL || grid == 0)
-	{
-		return;
-	}
-
-	for (y = 0; y < height;  y++)
-	{
-		free(grid[y]);
-	}
-
+	for (; i < height; i++)
+		free(grid[i]);
 	free(grid);
 }
