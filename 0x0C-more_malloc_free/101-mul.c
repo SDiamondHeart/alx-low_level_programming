dc44c2367fae9c526180021088168902449ad0c6
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-10-06 13:27:26 -0700

    all tasks

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two positive numbers
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ * @min: minimum number
+ * @max: maximum number
+ *
+ * Return: pointer to the newly created array
+ */
+
+int *array_range(int min, int max)
+{
+	int len, i;
+	int *ptr;
+
+	if (min > max)
+		return (NULL);
+	len = max - min + 1;
+	ptr = malloc(sizeof(int) * len);
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		ptr[i] = min++;
+	return (ptr);
+}
