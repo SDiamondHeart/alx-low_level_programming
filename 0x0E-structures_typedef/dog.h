43c8ca4a2e019431012b9a6d46351603cfd7772d
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-10-10 12:03:46 -0700

    tasks

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef __dog_h__
+#define __dog_h__
+
+/**
+ * struct dog - new function type on dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return:0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
