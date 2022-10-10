71ee439184172298136904fb51558ca117b3e370
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-10-10 12:30:15 -0700

    tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,13 @@
-#ifndef __dog_h__
-#define __dog_h__
+#ifndef __DOG_H__
+#define __DOG_H__
 
 /**
- * struct dog - new function type on dogs
+ * struct dog - information on dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
  *
- * Return:0
+ * Return: 0
  */
 
 struct dog
@@ -16,3 +16,15 @@ struct dog
 	float age;
 	char *owner;
 };
+
+int _putchar(char);
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif /* __DOG_H__ */
