86c3e60be84c9b7902dfff04296c5611df3cb9ca
Amarachukwu Ezema <ezemaa41@gmail.com>
2022-10-12 06:54:23 -0700

    undone

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,11 @@
-#ifndef __FUNCTION_POINTERS_H__
-#define __FUNCTION_POINTERS_H__
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 
-int _putchar(char c);
+#include <stddef.h>
 
+int _putchar(char);
 void print_name(char *name, void (*f)(char *));
-
 void array_iterator(int *array, size_t size, void (*action)(int));
-
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /* __FUNCTION_POINTERS_H__ */
+#endif /* _FUNCTION_POINTERS_H_ */
