b76545520d7a579e7c1404675f31af8c965ac51f
Amarachukwu Ezema <ezemaa41@gmail.com>
2023-02-20 02:28:12 -0800

    tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point of the code
+ *
+ * Return - 0 on success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
