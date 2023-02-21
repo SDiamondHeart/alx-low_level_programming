88c0c180e360ae33a13532d5bd2ef54c5557eafa
Amarachukwu Ezema <ezemaa41@gmail.com>
2023-02-20 17:11:18 -0800

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 /**
  * main - the entry point of the code
@@ -8,6 +9,6 @@
  */
 int main(void)
 {
-	write(2, "and that price of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	write(STDOUT_FILENO, "and that price of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
