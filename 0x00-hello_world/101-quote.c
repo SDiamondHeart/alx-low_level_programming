653f5c92cf9be0ee751c656499308d1fc3fe9cb4
Amarachukwu Ezema <ezemaa41@gmail.com>
2023-02-20 16:37:42 -0800

    tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - the entry point of the code
+ *
+ * Return: 0 on success, otherwise 1 exactly to standard error
+ */
+int main(void)
+{
+	write(2, "and that price of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
